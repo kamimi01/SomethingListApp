b1a6ef4115df7530583feeaa3cb0e68917c925c9
ibm_jp370627 <jp370627@ibm.com>
2019-11-13 23:08:53 +0900

    Add basic logic

---
+++ app.js
@@ -0,0 +1,21 @@
+// モジュールの読み込み
+var express = require('express'),
+    app = express();
+
+// routing
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+app.get('/about', function(req, res) {
+    res.send('about this page');
+});
+app.get('/users/:name?', function(req, res) {
+    if (req.params.name) {
+        res.send('hello, ' + req.params.name);
+    } else {
+        res.send('Hello, nobody!');
+    }
+});
+
+app.listen(3000);
+console.log("Server Starting...");
